19ba1ab75aff11a5e90a774bb1f5937d5f6bb363
Peter Biro <github@vlx.dk>
2021-01-13 23:10:22 +0100

    google analytics added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-
   render() {
     return (
       <Html>
         <Head />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SZ0CH728WN"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-SZ0CH728WN');
+          `,
+          }}
+        ></script>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap"
